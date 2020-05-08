0421897d5546187c81b5fe69dbef06ceb8f5cbbf
reginald160 <ozougwuifeanyi160@gmail.com>
2020-05-08 08:13:29 +0100

    .

---
+++ Labtask14-c#/printOdds1To255/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace printOdds1To255
+{
+	public delegate void printOdds1ToDele ();
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{      
+
+			printOdds1ToDele printOdds = () => {
+				int[] odd = { };
+				for (int i = 0; i < 255; i++) {
+					if (i % 2 != 0) {
+						Console.WriteLine (i);
+					}
+				}
+
+			};
+			printOdds ();
+		}
+	}
+}
